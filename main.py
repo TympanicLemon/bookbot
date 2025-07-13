bb86a9bff122fad1c6a1f9223c3f6e3e949a8abd
Daniel Zapata <zdaniel4771@gmail.com>
2025-07-12 23:26:55 -0700

    refactored main, to now take in arg from cli to use any path to a book in books/ dir

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_letter_counts, get_word_count, sort_counts
 
 def get_book_text(file_path):
@@ -6,14 +7,18 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-  book = "books/frankenstein.txt"
-  book_text = get_book_text(book)
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+  book_path = sys.argv[1]
+  book_text = get_book_text(book_path)
 
   count = get_word_count(book_text)
   letter_counts = get_letter_counts(book_text)
   sorted_counts = sort_counts(letter_counts)
   header = f"""============ BOOKBOT ============
-Analyzing book found at {book}...
+Analyzing book found at {book_path}...
 ----------- Word Count ----------"""
 
   print(header)
