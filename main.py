f22895f6b043f6ae1f2bbb8875ceef2a955d9f6e
Daniel Zapata <zdaniel4771@gmail.com>
2025-07-12 22:43:34 -0700

    added function to count how often each char appears in book txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_word_count
+from stats import get_letter_counts, get_word_count
 
 def get_book_text(file_path):
   with open(file_path) as f:
@@ -9,5 +9,10 @@ def main():
   book_text = get_book_text("books/frankenstein.txt")
   count = get_word_count(book_text)
   print(f"{count} words found in the document")
+  letter_counts = get_letter_counts(book_text)
+
+  print("\nCount for number of times each letter appears in the book:")
+  for char in letter_counts:
+    print(f"'{char}': {letter_counts[char]}")
 
 main()
--- stats.py
+++ stats.py
@@ -5,3 +5,19 @@ def get_word_count(book_text):
     count += 1
 
   return count
+
+def get_letter_counts(book_text):
+  letter_counts = {
+      'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0,
+      'k': 0, 'l': 0, 'm': 0, 'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0,
+      'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
+    }
+
+  words = book_text.split()
+  for word in words:
+    word = word.lower()
+    for char in word:
+      if char in letter_counts:
+        letter_counts[char] += 1
+
+  return letter_counts
